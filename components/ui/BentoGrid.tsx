4a4cc3c2e66558ca2467ef6e1bd823cebf13aafb
pouya <pouya.ff7@gmail.com>
2025-07-08 12:16:31 +0330

    Your commit5

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -68,7 +68,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "pouya.ff7@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
